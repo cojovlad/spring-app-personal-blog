76b8e022ba44bd0ea60258039eb3930d67fd5156
Vlad-Andrei Cojocariu <vlad-andrei.cojocariu@ibm.com>
2025-03-16 14:09:40 +0200

    HWVE-0000 initial creation of application

---
+++ src/main/java/com/example/spring_app_personal_blog/SpringAppPersonalBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_app_personal_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppPersonalBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppPersonalBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_app_personal_blog/SpringAppPersonalBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_app_personal_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppPersonalBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
